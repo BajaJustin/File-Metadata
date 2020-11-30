09197055584ad4b7ca5fc93ac2a806808d176a36
BajaJustin <justindabaja@gmail.com>
2020-11-30 17:36:21 -0500

    Added POST request to upload files

--- server.js
+++ server.js
@@ -13,6 +13,16 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  // req.file is the name of your file in the form above, here 'uploaded_file'
+  // req.body will hold the text fields, if there were any 
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
+})
+
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
